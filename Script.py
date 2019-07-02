9acc2967590dee997bc2f7a989ab4df7b59cf01e
Doga <doga.sari@metu.edu.tr>
2019-07-02 18:04:31 +0300

    Change Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,4 @@
-import matplotlib.pyplot as mpl
+def main(msg):
+    print(msg)
 
-mpl.plot([1, 2, 3, 4])
-mpl.ylabel('some numbers')
-mpl.xlabel('some other numbers')
-mpl.show()
-
+main("Hello World!")   
